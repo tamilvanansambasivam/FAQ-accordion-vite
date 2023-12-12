9bc26349f2775e9e57b4396a894fe463dadb5a4c
tamilvanan <tamilvanansambasivam@gmail.com>
2023-12-12 18:40:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <>
+      <h1> FAQs</h1>
+      <h2> What is Frontend Mentor, and how will it help me?</h2>{" "}
+      <p>
+        Frontend Mentor offers realistic coding challenges to help developers
+        improve their frontend coding skills with projects in HTML, CSS, and
+        JavaScript. It's suitable for all levels and ideal for portfolio
+        building.
+      </p>{" "}
+      <h2> Is Frontend Mentor free? </h2>
+      <p>
+        Yes, Frontend Mentor offers both free and premium coding challenges,
+        with the free option providing access to a range of projects suitable
+        for all skill levels.
+      </p>{" "}
+      <h2> Can I use Frontend Mentor projects in my portfolio?</h2>{" "}
+      <p>
+        Yes, you can use projects completed on Frontend Mentor in your
+        portfolio. It's an excellent way to showcase your skills to potential
+        employers!{" "}
+      </p>
+      <h2>How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>{" "}
+      <p>
+        {" "}
+        The best place to get help is inside Frontend Mentor's Discord
+        community. There's a help channel where you can ask questions and seek
+        support from other community members.
+      </p>
+    </>
+  );
+}
+
+export default App;
